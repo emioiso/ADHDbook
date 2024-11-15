dd98b395937dbdf43bcd79235def79bce80e2cf6
emioiso <jijjjiijiijijiji@gmail.com>
2024-11-16 05:18:28 +0900

    https://zenn.dev/rikutosato/books/fd565ea7ec70ec/viewer/fe7d74ここまでは正常に動いている

---
+++ ADHDbook/scoreDistractionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  scoredistractionViewController.swift
+//  ADHDbook
+//
+//  Created by emi oiso on 2024/11/16.
+//
+
+import UIKit
+
+class scoredistractionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
