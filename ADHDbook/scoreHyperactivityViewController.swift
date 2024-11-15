843694905212225cd4a9417607396cad570fe22c
emioiso <jijjjiijiijijiji@gmail.com>
2024-11-16 07:41:07 +0900

    scoreHyperactivityViewControllerが定義されていないらしい。

---
+++ ADHDbook/scoreHyperactivityViewController.swift
@@ -0,0 +1,8 @@
+//
+//  hyperactivity.swift
+//  ADHDbookUITests
+//
+//  Created by emi oiso on 2024/11/16.
+//
+
+import Foundation
---
+++ ADHDbook/scoreImpulsivityViewController.swift
@@ -0,0 +1,18 @@
+//
+//  scoreImpulsivityViewController.swift
+//  ADHDbook
+//
+//  Created by emi oiso on 2024/11/16.
+//
+
+import SwiftUI
+
+struct scoreImpulsivityViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    scoreImpulsivityViewController()
+}
