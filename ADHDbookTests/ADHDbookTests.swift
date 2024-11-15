9a67ff6ab12521e2f7fba70cacda980adf6c8e69
emioiso <jijjjiijiijijiji@gmail.com>
2024-11-16 04:28:14 +0900

    Initial Commit

---
+++ ADHDbookTests/ADHDbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDbookTests.swift
+//  ADHDbookTests
+//
+//  Created by emi oiso on 2024/11/16.
+//
+
+import Testing
+@testable import ADHDbook
+
+struct ADHDbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
